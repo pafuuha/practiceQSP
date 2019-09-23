0ee49ae7fee0ccc2aab3cf9a99f5fb3ae6e1acc8
パフーハ <pafuuha45542@gmail.com>
2019-09-23 13:48:29 +0900

    MortonOrderのTestを追加

---
+++ practiceQSP/Assets/QSP/ConverMortonOrder.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ConverMortonOrder 
+{
+    /// <summary>
+    /// モートン順列を返す
+    /// </summary>
+    /// <param name="x">Xの座標</param>
+    /// <param name="y">Yの座標</param>
+    /// <param name="lengthX">１単位のXの長さ</param>
+    /// <param name="lengthY">１単位のYの長さ</param>
+    /// <returns></returns>
+    public static uint GetMortonOrder(float x, float y, int lengthX, int lengthY)
+    {
+        int numX = (int)x / lengthX;
+        int numY = (int)y / lengthY;
+        
+
+        return bitSeparate((uint)numX) | bitSeparate((uint)numY) << 1;
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="target"></param>
+    /// <param name="lengthX"></param>
+    /// <param name="lengthY"></param>
+    /// <returns></returns>
+    public static uint GetSpriteRendererMortonOrder(SpriteRenderer target, int lengthX, int lengthY)
+    {
+
+        float targetX = target.transform.position.x;
+        float targetY = target.transform.position.y;
+
+        float targetW = target.size.x;
+        float targetH = target.size.y;
+
+        uint mortonOrderLeftUp = GetMortonOrder(targetX - targetW / 2, targetY - targetH / 2, lengthX, lengthY);
+        uint mortonOrderRightDown = GetMortonOrder(targetX + targetW / 2, targetY - targetH / 2, lengthX, lengthY);
+
+        return mortonOrderLeftUp & mortonOrderRightDown;
+    }
+
+    private static uint bitSeparate(uint inp)
+    {
+        inp = (inp | (inp << 8)) & 0x00ff00ff;
+        inp = (inp | (inp << 4)) & 0x0f0f0f0f;
+        inp = (inp | (inp << 2)) & 0x33333333;
+        inp = (inp | (inp << 1)) & 0x55555555;
+        return inp;
+    }
+}
---
+++ practiceQSP/Assets/QSP/Tests/MortonOrderTestScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class MortonOrderTestScript
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void MortonOrder_0_0()
+        {
+           uint morton =  ConverMortonOrder.GetMortonOrder(0,0,1,1);
+            Assert.AreEqual(0,morton);
+            // Use the Assert class to test conditions
+        }
+
+        [Test]
+        public void MortonOrder_1_1()
+        {
+            uint morton = ConverMortonOrder.GetMortonOrder(1, 1, 1, 1);
+            Assert.AreEqual(0b11, morton);
+        }
+
+        [Test]
+        public void MortonOrder_3_1()
+        {
+            uint morton = ConverMortonOrder.GetMortonOrder(3, 1, 1, 1);
+            Assert.AreEqual(0b0111, morton);
+        }
+        [Test]
+        public void MortonOrderDeci_3_1()
+        {
+            uint morton = ConverMortonOrder.GetMortonOrder(32, 16, 10, 10);
+            Assert.AreEqual(0b0111, morton);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator NewTestScriptWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
