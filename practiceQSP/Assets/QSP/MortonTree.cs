5570104811d352555d13656d81af26f6bee5b5c9
パフーハ <pafuuha45542@gmail.com>
2019-09-23 16:17:48 +0900

    木構造のクラスを作成

---
+++ practiceQSP/Assets/QSP/MortonTree.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MortonTree : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class TreeObject <T>
+{
+    public List<T> haveObjects = new List<T>();
+    public List<TreeObject<T>> pNext = new List<TreeObject<T>>();
+    public List<TreeObject<T>> pPrev = new List<TreeObject<T>>();
+
+    
+}
+
+public class test
+{
+    public void Init()
+    {
+        var root = new TreeObject<SpriteRenderer>();
+        
+
+    }
+}
